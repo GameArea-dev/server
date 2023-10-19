b86a7de292dc225e821c7efe46d6dba5282546fb
Freezlex <sonyc098@gmail.com>
2023-10-19 23:00:45 +0200

    feat: Add default config

---
+++ src/main/kotlin/fr/gamearea/Application.kt
@@ -0,0 +1,20 @@
+package fr.gamearea
+
+import fr.gamearea.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+            .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureSerialization()
+    // configureDatabases()
+    configureSockets()
+    configureRouting()
+}
---
+++ src/main/kotlin/fr/gamearea/plugins/Security.kt
@@ -0,0 +1,6 @@
+package fr.gamearea.plugins
+
+import io.ktor.server.application.*
+
+fun Application.configureSecurity() {
+}
